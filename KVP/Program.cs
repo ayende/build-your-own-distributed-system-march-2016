550f4fd35676ac151b998c8f44e914a8c6a2b209
Ayende Rahien <Ayende@ayende.com>
2016-03-31 12:07:57 -0300

    Can now read/write name

--- KVP/Program.cs
+++ KVP/Program.cs
@@ -1,4 +1,7 @@
-﻿using Voron;
+﻿using System;
+using System.Text;
+using Voron;
+using Voron.Trees;
 
 namespace KVP
 {
@@ -9,7 +12,27 @@ namespace KVP
             var options = StorageEnvironmentOptions.CreateMemoryOnly();
             using (var env = new StorageEnvironment(options))
             {
-                
+                using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
+                {
+                    env.CreateTree(tx, "Values");
+
+                    tx.Commit();
+                }
+                using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
+                {
+                    var tree = tx.ReadTree("Values");
+                    tree.Add("Oren", Encoding.UTF8.GetBytes("Eini"));
+                    tx.Commit();
+                }
+
+                using (var tx = env.NewTransaction(TransactionFlags.Read))
+                {
+                    var tree = tx.ReadTree("Values");
+                    var readResult = tree.Read("Oren");
+                    Console.WriteLine(readResult.Reader.ReadAllAsString());
+
+                    tx.Commit();
+                }
 
 
             }
