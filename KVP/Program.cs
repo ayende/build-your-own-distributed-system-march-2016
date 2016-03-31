fa73cd9794c458d07b62a79c4c7e41ac6b829400
Ayende Rahien <Ayende@ayende.com>
2016-03-31 15:26:34 -0300

    Use numeric bytes for the ids Count of votes per color

--- KVP/Program.cs
+++ KVP/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Voron;
 using Voron.Trees;
+using Voron.Util.Conversion;
 
 namespace KVP
 {
@@ -16,11 +17,12 @@ namespace KVP
                 {
                     env.CreateTree(tx, "Votes");
                     env.CreateTree(tx, "CityVotes");
+                    env.CreateTree(tx, "VotesCounts");
 
                     tx.Commit();
                 }
                 VoteFor(env, 1304, "Elemar", "Black", "Sao Paulo");
-                VoteFor(env, 34, "Filipe", "Black", "Sao Paulo");
+                VoteFor(env, 34, "Filipe", "Blue", "Sao Paulo");
                 VoteFor(env, 304, "Ariel", "Black", "New York");
 
                 using (var tx = env.NewTransaction(TransactionFlags.Read))
@@ -32,26 +34,60 @@ namespace KVP
                         {
                             do
                             {
-                                var id = it.CurrentKey.CreateReader().ReadAllAsString();
+                                var id = it.CurrentKey.CreateReader().ReadBigEndianInt32();
                                 var value = it.CreateReaderForCurrent().ReadAllAsString();
                                 Console.WriteLine(id + "," + value);
                             } while (it.MoveNext());
                         }
                     }
-                    Console.WriteLine();
-                    Console.WriteLine("Votes in SP:");
+                    //Console.WriteLine();
+
+                    //var votesCounts = tx.ReadTree("VotesCounts");
+
+                    //using (var it = votesCounts.Iterate())
+                    //{
+                    //    if (it.Seek(Slice.BeforeAllKeys))
+                    //    {
+                    //        do
+                    //        {
+                    //            var color = it.CurrentKey.CreateReader().ReadAllAsString();
+                    //            var value = it.CreateReaderForCurrent().ReadLittleEndianInt64();
+
+                    //            Console.WriteLine($"{color} = {value}");
+                    //        } while (it.MoveNext());
+                    //    }
+                    //}
+                    //Console.WriteLine();
+                    //Console.WriteLine("Votes in SP:");
+
+
                     var cityVotes = tx.ReadTree("CityVotes");
-                    using (var it = cityVotes.MultiRead("Sao Paulo"))
+                    Console.WriteLine(cityVotes.State.EntriesCount);
+                    using (var it = cityVotes.Iterate())
                     {
                         if (it.Seek(Slice.BeforeAllKeys))
                         {
                             do
                             {
-                                var readResult = votes.Read(it.CurrentKey);
-                                Console.WriteLine(readResult.Reader.ReadAllAsString());
+                                Console.WriteLine(
+                                    it.CurrentKey.CreateReader().ReadAllAsString() + " "
+                                    +
+                                cityVotes.MultiCount(it.CurrentKey));
+                                
                             } while (it.MoveNext());
                         }
                     }
+                    //using (var it = cityVotes.MultiRead("Sao Paulo"))
+                    //{
+                    //    if (it.Seek(Slice.BeforeAllKeys))
+                    //    {
+                    //        do
+                    //        {
+                    //            var readResult = votes.Read(it.CurrentKey);
+                    //            Console.WriteLine(readResult.Reader.ReadAllAsString());
+                    //        } while (it.MoveNext());
+                    //    }
+                    //}
                 }
 
 
@@ -67,12 +103,19 @@ namespace KVP
                 if (tree.Read(id.ToString()) != null)
                     return;
 
-                tree.Add(id.ToString(),
+                var key = new Slice(EndianBitConverter.Big.GetBytes(id));
+                tree.Add(key, 
                     Encoding.UTF8.GetBytes(name + "," + color + "," + city));
 
                 var cityVotes = tx.ReadTree("CityVotes");
                 // var cityVotes = new Dictionary<string, HashSet<string>>();
-                cityVotes.MultiAdd(city, id.ToString());
+                cityVotes.MultiAdd(city, key);
+
+                var votesCounts = tx.ReadTree("VotesCounts");
+
+                var c = votesCounts.Increment(color, 1);
+
+                //Console.WriteLine($"{c} votes for {color}");
 
                 tx.Commit();
             }
