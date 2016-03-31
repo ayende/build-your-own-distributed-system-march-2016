47ca17020e032b835713923122ce2d43ae7e073a
Ayende Rahien <Ayende@ayende.com>
2016-03-31 14:47:27 -0300

    Get the raw data from the tree after reading the index

--- KVP/Program.cs
+++ KVP/Program.cs
@@ -19,14 +19,14 @@ namespace KVP
 
                     tx.Commit();
                 }
-                VoteFor(env, 1304, "Black", "Sao Paulo");
-                VoteFor(env, 34, "Black", "Sao Paulo");
-                VoteFor(env, 304, "Black", "New York");
+                VoteFor(env, 1304, "Elemar", "Black", "Sao Paulo");
+                VoteFor(env, 34, "Filipe", "Black", "Sao Paulo");
+                VoteFor(env, 304, "Ariel", "Black", "New York");
 
                 using (var tx = env.NewTransaction(TransactionFlags.Read))
                 {
-                    var tree = tx.ReadTree("Votes");
-                    using (var it = tree.Iterate())
+                    var votes = tx.ReadTree("Votes");
+                    using (var it = votes.Iterate())
                     {
                         if (it.Seek(Slice.BeforeAllKeys))
                         {
@@ -34,7 +34,7 @@ namespace KVP
                             {
                                 var id = it.CurrentKey.CreateReader().ReadAllAsString();
                                 var value = it.CreateReaderForCurrent().ReadAllAsString();
-                                Console.WriteLine(id+","+value);
+                                Console.WriteLine(id + "," + value);
                             } while (it.MoveNext());
                         }
                     }
@@ -47,7 +47,8 @@ namespace KVP
                         {
                             do
                             {
-                                Console.WriteLine(it.CurrentKey.CreateReader().ReadAllAsString());
+                                var readResult = votes.Read(it.CurrentKey);
+                                Console.WriteLine(readResult.Reader.ReadAllAsString());
                             } while (it.MoveNext());
                         }
                     }
@@ -57,7 +58,7 @@ namespace KVP
             }
         }
 
-        private static void VoteFor(StorageEnvironment env, int id, string color, string city)
+        private static void VoteFor(StorageEnvironment env, int id, string name, string color, string city)
         {
             using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
             {
@@ -67,7 +68,7 @@ namespace KVP
                     return;
 
                 tree.Add(id.ToString(),
-                    Encoding.UTF8.GetBytes(color + "," + city));
+                    Encoding.UTF8.GetBytes(name + "," + color + "," + city));
 
                 var cityVotes = tx.ReadTree("CityVotes");
                 // var cityVotes = new Dictionary<string, HashSet<string>>();
