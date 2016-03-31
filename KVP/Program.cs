f33f2274876b8e9f26bd7e2681cdef23b9b38e87
Ayende Rahien <Ayende@ayende.com>
2016-03-31 12:58:23 -0300

    Starting to build elections

--- KVP/Program.cs
+++ KVP/Program.cs
@@ -9,32 +9,51 @@ namespace KVP
     {
         static void Main(string[] args)
         {
-            var options = StorageEnvironmentOptions.CreateMemoryOnly();
+            var options = StorageEnvironmentOptions.ForPath("test");
             using (var env = new StorageEnvironment(options))
             {
                 using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
                 {
-                    env.CreateTree(tx, "Values");
+                    env.CreateTree(tx, "Votes");
 
                     tx.Commit();
                 }
-                using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
-                {
-                    var tree = tx.ReadTree("Values");
-                    tree.Add("Oren", Encoding.UTF8.GetBytes("Eini"));
-                    tx.Commit();
-                }
+
+                VoteFor(env, 304, "Black", "Sao Paulo");
 
                 using (var tx = env.NewTransaction(TransactionFlags.Read))
                 {
-                    var tree = tx.ReadTree("Values");
-                    var readResult = tree.Read("Oren");
-                    Console.WriteLine(readResult.Reader.ReadAllAsString());
-
-                    tx.Commit();
+                    var tree = tx.ReadTree("Votes");
+                    using (var it = tree.Iterate())
+                    {
+                        if (it.Seek(Slice.BeforeAllKeys))
+                        {
+                            do
+                            {
+                                var id = it.CurrentKey.CreateReader().ReadAllAsString();
+                                var value = it.CreateReaderForCurrent().ReadAllAsString();
+                                Console.WriteLine(id+","+value);
+                            } while (it.MoveNext());
+                        }
+                    }
                 }
 
 
+            }
+        }
+
+        private static void VoteFor(StorageEnvironment env, int id, string color, string city)
+        {
+            using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                var tree = tx.ReadTree("Votes");
+
+                if (tree.Read(id.ToString()) != null)
+                    return;
+
+                tree.Add(id.ToString(),
+                    Encoding.UTF8.GetBytes(color + "," + city));
+                tx.Commit();
             }
         }
     }
